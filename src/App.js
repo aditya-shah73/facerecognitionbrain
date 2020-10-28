f089b4920f4a78dd149b3220d551cec1aee48e64
Aditya Shah <aditya7395@gmail.com>
2020-10-28 11:47:13 -0700

    Updated Clarifai model and finxed the image rendering issue

--- src/App.js
+++ src/App.js
@@ -42,11 +42,11 @@ class App extends React.Component {
     console.log("Clicking the button");
     app.models
       .predict(
-        Clarifai.COLOR_MODEL,
+        Clarifai.FACE_DETECT_MODEL,
         this.state.input)
       .then(
         function(response){
-          console.log(response);
+          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
         },
         function(err){
 
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -2,8 +2,10 @@ import React from  'react';
 
 const FaceRecognition = ({imageUrl}) => {
   return(
-    <div className = 'center'>
-      <img alt='test' src ={imageUrl} />
+    <div className = 'center ma'>
+      <div className = 'absolute mt2'>
+        <img alt='test' src ={imageUrl} width='500px' height='auto'/>
+      </div>
     </div>
   );
 }
