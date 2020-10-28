6caec824cd00b26bae0dddb425bcd322a113c44c
Aditya Shah <aditya7395@gmail.com>
2020-10-28 09:59:14 -0700

    Added the rank component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
+import React from 'react';
 import './App.css';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <ImageLinkForm />
-    </div>
-  );
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+      </div>
+    );
+  }
 }
 
 export default App;
